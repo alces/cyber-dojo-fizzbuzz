1758c4e7fb477a69b4a0e9c7a67445a2701e087a
gvptaF <gvptaF@cyber-dojo.org>
2024-05-14 12:18:50 +0000

    41 ran tests, predicted green, got red

--- files/numbers.go
+++ files/numbers.go
@@ -2,7 +2,10 @@ package fizzbuzz
 
 func numbers(length int, modificator func (int) string) []string {
     result := make([]string, length)
+
     for i := 0; i < length; i++ {
         result[i] = modificator(i)
     }
+    
+    return result
 }
