35a9102d021492712689650bc841d2f758561866
gvptaF <gvptaF@cyber-dojo.org>
2024-05-14 12:22:42 +0000

    46 ran tests, predicted green, got green

--- files/numbers.go
+++ files/numbers.go
@@ -4,7 +4,7 @@ func numbers(length int, modificator func (int) string) []string {
     result := make([]string, length)
 
     for i := 0; i < length; i++ {
-        result[i] = modificator(i)
+        result[i] = modificator(i + 1)
     }
     
     return result
