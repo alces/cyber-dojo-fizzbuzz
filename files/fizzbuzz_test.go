eae0af58f4a271ce02c23ec057576000c1982cda
gvptaF <gvptaF@cyber-dojo.org>
2024-05-14 08:29:50 +0000

    18 ran tests, predicted green, got green

--- files/fizzbuzz_test.go
+++ files/fizzbuzz_test.go
@@ -5,12 +5,10 @@ import (
     "testing"
 )
 
-type testResult struct {
+var testData = []struct {
     argument int
     expected string
-}
-
-var testData = []testResult{
+}{
     {argument: 2, expected: "2"},
     {argument: 3, expected: "Fizz"},
     {argument: 11, expected: "11"},
@@ -18,10 +16,9 @@ var testData = []testResult{
 }
 
 func TestFizzBuzz(t *testing.T) {
-    for _, result := range testData {
-        actual := FizzBuzz(result.argument)
-        if actual != result.expected {
-            t.Error(fmt.Sprintf("Expected %#v while actual is %#v", result.expected, actual))
+    for _, res := range testData {        
+        if actual := FizzBuzz(res.argument); actual != res.expected {
+            t.Error(fmt.Sprintf("Expected %#v while actual is %#v", res.expected, actual))
         }
     }
 }
